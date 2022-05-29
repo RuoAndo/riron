f0e7ac8e82443d8610f110b3da7e1a6200873dd7
Ruo Ando <ando.ruo@gmail.com>
2022-05-29 09:44:57 +0900

    shuffle

--- shuffle.c
+++ shuffle.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <iostream>
 #include <bitset>
+#include <time.h>
 
 void shuffle(int array[], int size) {
     for(int i = 0; i < size; i++) {
-        int j = rand()%size;
-        int t = array[i];
-        array[i] = array[j];
-        array[j] = t;
+      srand((unsigned int)time(NULL));
+      int j = rand() % size;
+        // int j = rand()%size;
+      int t = array[i];
+      array[i] = array[j];
+      array[j] = t;
     }
 }
 
